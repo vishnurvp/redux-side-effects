03b88f74efa57491105878066e14c6bbcdfaff69
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-07-18 15:10:10 +0530

    Task_20

--- src/App.js
+++ src/App.js
@@ -1,16 +1,82 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import {useSelector} from 'react-redux'
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useDispatch, useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { uiActions } from "./GlobalStore/uiReducer";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
-  const isCartOpen = useSelector(state=>state.cart.isCartOpen);
+  const dispatch = useDispatch();
+  const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data",
+        })
+      );
+      const response = await fetch(
+        `https://react-http-f04a8-default-rtdb.firebaseio.com/cart.json`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("sending cart data failed.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {isCartOpen && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {isCartOpen && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/GlobalStore/index.js
+++ src/GlobalStore/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cartReducer'
+import uiReducer from './uiReducer';
 
 const store = configureStore({
     reducer: {
         cart: cartReducer,
+        ui: uiReducer,
     }
 });
 
